d6379f0098bdbdb645efaa61995a17c385d787ff
Test User <test@thulani-malatji.test.entrostat.xyz>
2021-12-15 07:07:29 +0000

    create a connection to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,10 @@ import { MailModule } from './mail/mail.module';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mongodb',
-      url: process.env.MONGODB_CONNECTION_STRING,
+      host: process.env.MONGODB_HOST,
+      port: 27017,
+      username: process.env.MONGODB_USERNAME,
+      password: process.env.MONGODB_PASSWORD,
       database: process.env.MONGODB_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       ssl: true,
